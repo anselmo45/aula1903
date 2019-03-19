a918b9007949cc6b2eb87db13ab7abde508adb5c
Anselmo Luiz <anselmoluizd@gmail.com>
2019-03-19 20:08:11 -0300

    commit inicial

---
+++ src/aula1903.java
@@ -0,0 +1,19 @@
+
+import static java.time.Clock.system;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Anselmo Rocha
+ */
+public class aula1903 {
+    public static void main(String[] args) {
+        int a =5;
+    }
+ 
+}
